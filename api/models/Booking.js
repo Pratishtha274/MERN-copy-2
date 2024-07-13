c943885e2ccb1817a934820cf1839f5a1a73ed5f
Pratishtha274 <146354857+Pratishtha274@users.noreply.github.com>
2024-07-13 13:54:39 +0530

    Remove nested repositories and add clean directories

---
+++ api/models/Booking.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const bookingSchema =new mongoose.Schema({
+    service: {type:mongoose.Schema.Types.ObjectId,required:true,ref:'service'},
+    user: {type:mongoose.Schema.Types.ObjectId,required:true},
+    bookingDate: {type:Date,},
+    name: {type:String},
+    mobile: {type:String},
+    addr: {type:String},
+    price: Number,
+});
+
+const BookingModel = mongoose.model('Booking',bookingSchema);
+
+module.exports = BookingModel;
