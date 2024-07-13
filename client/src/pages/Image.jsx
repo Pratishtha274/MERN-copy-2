4c04e84db5846f4ad4e45cd020b791f6884dfad3
Pratishtha274 <146354857+Pratishtha274@users.noreply.github.com>
2024-07-13 19:20:50 +0530

    new commit 2

--- client/src/pages/Image.jsx
+++ client/src/pages/Image.jsx
@@ -3,7 +3,10 @@ export default function Image({src,...rest}) {
    /*if (src && src.startsWith('http://localhost:4000/uploads')) {
       src = src.replace('http://localhost:4000/uploads', 'https://pratishtha.vercel.app/uploads');
   }*/
-  
+      const isLocal = src && src.includes('http://localhost:4000/uploads/');
+      const secureSrc = isLocal
+        ? src.replace('http://localhost:4000/uploads/', 'https://pratishtha.vercel.app/uploads/')
+        : src;
    return(
     <img {...rest} src={src} alt={''} />
    )
