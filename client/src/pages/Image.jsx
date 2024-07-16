799b36733ef25f391ba78b6ff0683e101b183a89
Pratishtha274 <146354857+Pratishtha274@users.noreply.github.com>
2024-07-16 23:41:13 +0530

    uploads url

--- client/src/pages/Image.jsx
+++ client/src/pages/Image.jsx
@@ -70,7 +70,7 @@ export default function Image({ src, ...rest }) {
 
     if (src) {
         const isLocal = src.includes('http://localhost:4000/uploads/');
-        const isS3 = src.includes('https://pratishtha-booking-app.s3.ap-southeast-2.amazonaws.com/');
+        const isS3 = src.includes('https://pratishtha-booking-app.s3.ap-southeast-2.amazonaws.com/uploads/');
 
         if (isLocal) {
             src = src.replace('http://localhost:4000/uploads/', uploadsBaseURL);
