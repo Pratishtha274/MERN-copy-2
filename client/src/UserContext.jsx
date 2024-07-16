1a0892e77ffcb538b28cd7b37a0520e6aebf0ee7
Pratishtha274 <146354857+Pratishtha274@users.noreply.github.com>
2024-07-16 20:51:55 +0530

    change endpoint

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -9,7 +9,7 @@ export function UserContextProvider({ children }) {
 
     useEffect(() => {
         if (!user) {
-            axios.get('/api/profile')
+            axios.get('/profile')
                 .then(({ data }) => {
                     setUser(data);
                     setReady(true);
